c205d5dc80b09ed61c33132d1fa07750ca23739c
Özgür <ozgurturkiye@gmail.com>
2024-11-19 02:11:50 +0300

    feat: add AuthorDetailApi as view

--- apis/urls.py
+++ apis/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from .views import AuthorListApi
+from .views import AuthorListApi, AuthorDetailApi
 
 
 author_patterns = [
     path("", AuthorListApi.as_view(), name="author-list"),
-    # path("<int:pk>", AuthorListApi.as_view(), name="author-detail"),
+    path("<int:pk>", AuthorDetailApi.as_view(), name="author-detail"),
 ]
 
 urlpatterns = [
--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,6 @@
+from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 
 from catalog.models import Author
@@ -33,3 +34,48 @@ class AuthorListApi(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=201)
+
+
+class AuthorDetailApi(APIView):
+    """
+    Retrieve, update or delete a author instance.
+    """
+
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Author
+            fields = (
+                "id",
+                "first_name",
+                "last_name",
+                "date_of_birth",
+                "date_of_death",
+                "is_alive",
+            )
+
+    class InputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Author
+            fields = ("first_name", "last_name", "date_of_birth", "date_of_death")
+
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = self.OutputSerializer(author)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = self.InputSerializer(author, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def delete(self, request, pk, format=None):
+        author = self.get_object(pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
